afdb04fef5bb8ea99e982c287a821a7d2b9b6d6f
Niccolò Ajroldi <61059403+Niccolo-Ajroldi@users.noreply.github.com>
2025-03-17 09:44:14 +0100

    Update slurm.sh

--- cluster/multi_gpu/slurm.sh
+++ cluster/multi_gpu/slurm.sh
@@ -5,8 +5,8 @@
 #SBATCH --output=/ptmp/najroldi/logs/plainLM/out/%x_%A_%a.out
 #SBATCH --time=00:15:00
 #SBATCH --requeue
-#SBATCH --gres=gpu:a100:4
-#SBATCH --cpus-per-task=48
+#SBATCH --gres=gpu:a100:8
+#SBATCH --cpus-per-task=32
 #SBATCH --mem=500000
 #SBATCH --array=1
 
@@ -21,6 +21,6 @@ job_idx=$((SLURM_ARRAY_TASK_ID - 1))
 
 # Execute python script
 torchrun \
-  --redirects 1:0,2:0,3:0 \
-  --standalone --nnodes=1 --nproc_per_node=4 \
+  --redirects 1:0,2:0,3:0,4:0,5:0,6:0,7:0 \
+  --standalone --nnodes=1 --nproc_per_node=8 \
   train.py --config=$config --job_idx=$job_idx
