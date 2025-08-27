abdfcfc1224fb750ff759b995f009ec269513659
aldakata <ac.tatjer@gmail.com>
2025-08-27 23:23:01 +0200

    personal configs

---
+++ cluster/multi_gpu_sizes/condor4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+source /home/atatjer/src/plainLM/.venv/bin/activate
+
+# Job specific vars
+config=$1
+job_idx=$2  # CONDOR job array index ranges from 0 to n-1
+job_cluster=$3  # CONDOR job cluster
+
+export HOME=/home/atatjer
+export TMPDIR=/fast/atatjer/tmp/${job_cluster}/${job_idx}
+mkdir -p "$TMPDIR"
+
+# Launch torch distributed run on 8 devices
+torchrun \
+  --redirects 1:0,2:0,3:0,4:0,5:0,6:0,7:0 \
+  --standalone --nnodes=1 --nproc_per_node=4  train.py --config=$config --job_idx=$job_idx --job_cluster=$job_cluster
---
+++ cluster/multi_gpu_sizes/condor_local.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+source /home/atatjer/src/plainLM/.venv/bin/activate
+
+# Job specific vars
+config=$1
+job_idx=$2  # CONDOR job array index ranges from 0 to n-1
+job_cluster=$3  # CONDOR job cluster
+
+export HOME=/home/atatjer
+export TMPDIR=/fast/atatjer/tmp/${job_cluster}/${job_idx}
+mkdir -p "$TMPDIR"
+
+# Launch torch distributed run on 8 devices
+python train.py --config=$config --job_idx=$job_idx --job_cluster=$job_cluster
