f4b61c7906d4cf817d5acbb95742e7bda04a4c6d
Niccolo-Ajroldi <hgf_omt7140@hkn1990.localdomain>
2025-10-28 11:31:39 +0100

    clean scripts

--- cluster/multi_gpu/slurm.sh
+++ cluster/multi_gpu/slurm.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
+#SBATCH --account=p0023364
 #SBATCH --job-name=plainLM_test
-#SBATCH --error=/ptmp/najroldi/logs/plainLM/err/%x_%A_%a.err
-#SBATCH --output=/ptmp/najroldi/logs/plainLM/out/%x_%A_%a.out
+#SBATCH --error=/home/hk-project-p0023364/hgf_omt7140/log/%x_%A_%a.err
+#SBATCH --output=/home/hk-project-p0023364/hgf_omt7140/log/%x_%A_%a.out
 #SBATCH --time=00:15:00
 #SBATCH --requeue
-#SBATCH --gres=gpu:a100:8
+#SBATCH --gres=gpu:4
 #SBATCH --cpus-per-task=32
+#SBATCH --partition=dev_accelerated
 #SBATCH --mem=500000
 #SBATCH --array=1
 
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate plainLM
+# Activate environment
+cd ~/plainLM
+source .venv/bin/activate
 
 # Hyperparmeters are specified in a YAML configuration file
-config=config/config.yaml
+config=config/70M_10BT.yaml
 
 # SLURM job arrays range from 1 to n
 job_idx=$((SLURM_ARRAY_TASK_ID - 1))
 
 # Launch torch distributed run on 8 devices
 torchrun \
-  --redirects 1:0,2:0,3:0,4:0,5:0,6:0,7:0 \
-  --standalone --nnodes=1 --nproc_per_node=8 \
+  --redirects 1:0,2:0,3:0 \
+  --standalone --nnodes=1 --nproc_per_node=4 \
   train.py --config=$config --job_idx=$job_idx
--- cluster/single_gpu/slurm.sh
+++ cluster/single_gpu/slurm.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-#SBATCH --job-name=plainLM_test
-#SBATCH --error=/ptmp/najroldi/logs/plainLM/err/%x_%A_%a.err
-#SBATCH --output=/ptmp/najroldi/logs/plainLM/out/%x_%A_%a.out
+#SBATCH --account=hk-project-p0023364
+#SBATCH --job-name=test
+#SBATCH --error=/home/hk-project-p0023364/hgf_omt7140/log/%x_%A_%a.err
+#SBATCH --output=/home/hk-project-p0023364/hgf_omt7140/log/%x_%A_%a.out
 #SBATCH --time=00:15:00
 #SBATCH --requeue
-#SBATCH --gres=gpu:a100:1
-#SBATCH --cpus-per-task=12
-#SBATCH --mem=125000
-#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=32
+#SBATCH --partition=dev_accelerated
 #SBATCH --array=1
 
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate plainLM
+# Activate environment
+cd ~/plainLM
+source .venv/bin/activate
 
 # Hyperparmeters are specified in a YAML configuration file
-config=config/config.yaml
+config=config/70M_10BT.yaml
 
 # SLURM job arrays range from 1 to n
 job_idx=$((SLURM_ARRAY_TASK_ID - 1))
 
-# Execute python script
+# Launch torch distributed run on 8 devices
 python train.py --config=$config --job_idx=$job_idx
